ec514b2ae2fa30ef9a8464bfb6718ed3143cfbdf
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-19 20:49:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+let name=document.getElementById("name");
+let last=document.getElementById("last");
+let phone=document.getElementById("phone");
+let email=document.getElementById("email");
+let button=document.getElementById("submit");
+
+function text(){
+	let first=name.value;
+	let second=last.value;
+	let third=parseInt(phone.value);
+	let fourth=email.value;
+	alert(`First Name:${first} Last Name:${second} 
+	Phone Number:${third} 
+	Email:${fourth}`);
+}
+
+button.addEventListener("click",text);
+
